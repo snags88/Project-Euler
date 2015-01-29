47b8799db268892aeeac289433ec74054561fae3
Seiji Naganuma <Snaganumas@gmail.com>
2015-01-29 11:21:36 -0500

    add solution to Problem 17

--- Problem_17.rb
+++ Problem_17.rb
@@ -4,3 +4,50 @@
 
 
 # NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+
+
+teens_a = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+tens_a = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+single_a = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+letter_count = 0
+
+
+for num in 1..1000
+  number_string = ""
+  
+  if (num / 1000) > 0
+    number_string += single_a[(num / 1000) - 1]
+    number_string += "thousand"
+    num = num % 1000
+  end
+    
+  if (num / 100) > 0 
+    number_string += single_a[(num/100) - 1]
+    number_string += "hundred"
+    if num % 100 > 0
+      number_string += "and"
+    end
+    num = num % 100
+  end
+
+  if (num / 10) > 0
+    if (num / 10) == 1
+      number_string += teens_a[num - 10]
+      letter_count += number_string.length
+      puts number_string + " (" + number_string.length.to_s + ")"
+      next
+    else
+      number_string += tens_a[(num / 10) - 2]
+    end
+    num = num % 10
+  end
+
+  if num > 0
+    number_string += single_a[num - 1]
+  end
+  puts number_string + " (" + number_string.length.to_s + ")"
+  letter_count += number_string.length
+
+end
+
+puts letter_count
